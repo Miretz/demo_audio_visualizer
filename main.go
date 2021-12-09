a1301e780b1a3ecfccf6842b2d6a1f6a88e735c0
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-09 20:02:08 +0100

    updated methodology

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"math"
 	"os"
@@ -16,25 +17,15 @@ import (
 	"github.com/mjibson/go-dsp/window"
 )
 
-type FrequencyBand struct {
-	min int
-	max int
-}
-
-const numSamples = 4608
-const bitSize = 64
+const numSamples = 64
 const maxColumnWidth = 40
 const visualChar = "█"
-const magnitudeDivision = 12.0
-const peakFalloff = 1.2
+const emptyChar = " "
+const peakFalloff = 0.9
+const bitSize = 64
+const spectrumSize = 15
 
 var mutex sync.Mutex
-
-// most common frequency bands
-var freqBands = []FrequencyBand{
-	{30, 60}, {60, 90}, {90, 250}, {250, 300}, {300, 500}, // bass, low mid
-	{500, 750}, {750, 1000}, {1000, 2000}, {2000, 3000}, {3000, 4000}, // mid, high mid
-	{4000, 6000}, {6000, 8000}, {8000, 10000}, {10000, 20000}, {20000, 40000}} // high
 
 func play(filename string) error {
 
@@ -59,21 +50,25 @@ func play(filename string) error {
 	defer p.Close()
 
 	buf := make([]byte, numSamples)
-	freqSpectrum := make([]float64, len(freqBands))
+
+	freqSpectrum := make([]float64, spectrumSize)
 
 	screen.Clear()
 
 	for {
 		_, err := d.Read(buf)
 		if err != nil {
-			break
+			if err == io.EOF {
+				break
+			}
+			return err
 		}
-		p.Write(buf) // Playback
-
 		updateSpectrumValues(buf, d.SampleRate(), freqSpectrum)
 		go updateScreen(filename, freqSpectrum, &mutex)
-
+		p.Write(buf) // Playback
 	}
+
+	screen.Clear()
 
 	return nil
 }
@@ -86,27 +81,21 @@ func updateSpectrumValues(buffer []byte, sampleRate int, freqSpectrum []float64)
 	}
 
 	// apply window function
-	window.Apply(audioWave, window.Hann)
+	window.Apply(audioWave, window.Blackman)
 
 	// get the fft for each sample
 	fftOutput := fft.FFTReal(audioWave)
 
 	// get the magnitudes
-	for i := 0; i < numSamples/2-1; i++ {
+	for i := 0; i < spectrumSize; i++ {
 		fr := real(fftOutput[i])
 		fi := imag(fftOutput[i])
-		magnitude := math.Sqrt((fr * fr) + (fi * fi))
-		frequency := i * sampleRate / (numSamples / 2)
-		for bandIndex := 0; bandIndex < len(freqBands); bandIndex++ {
-			if frequency > freqBands[bandIndex].min && frequency <= freqBands[bandIndex].max {
-				val := math.Max(magnitude/magnitudeDivision, 0.0)
-				val = math.Min(maxColumnWidth, val)
-				if val > freqSpectrum[bandIndex] {
-					freqSpectrum[bandIndex] = val
-				} else {
-					freqSpectrum[bandIndex] = math.Max(freqSpectrum[bandIndex]-peakFalloff, 0)
-				}
-			}
+		magnitude := math.Sqrt(fr*fr + fi*fi)
+		val := math.Min(maxColumnWidth, math.Abs(magnitude))
+		if freqSpectrum[i] > val {
+			freqSpectrum[i] = math.Max(freqSpectrum[i]-peakFalloff, 0.0)
+		} else {
+			freqSpectrum[i] = (val + freqSpectrum[i]) / 2.0
 		}
 	}
 }
@@ -119,10 +108,15 @@ func updateScreen(filename string, spectrum []float64, m *sync.Mutex) {
 
 	// draw the columns to the console - will replace with a proper GUI library later
 	fmt.Println("> Now playing: ", filename)
-	for _, s := range spectrum {
-		fmt.Print(strings.Repeat(visualChar, int(s)))
-		fmt.Print(strings.Repeat(" ", maxColumnWidth-int(s)))
-		fmt.Println()
+	for i, s := range spectrum {
+		is := int(s)
+		if is == 0 {
+			fmt.Println()
+		} else {
+			fmt.Printf("%02d %02d %s%s\n", i, is,
+				strings.Repeat(visualChar, is),
+				strings.Repeat(emptyChar, maxColumnWidth-is))
+		}
 	}
 
 	m.Unlock()
