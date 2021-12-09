97d251fbc5f81828ef75e4f2cbeff3d52df13f03
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-09 21:13:33 +0100

    guy migration to raylib

--- main.go
+++ main.go
@@ -7,25 +7,22 @@ import (
 	"math"
 	"os"
 	"strconv"
-	"strings"
-	"sync"
 
+	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/hajimehoshi/go-mp3"
 	"github.com/hajimehoshi/oto"
-	"github.com/inancgumus/screen"
 	"github.com/mjibson/go-dsp/fft"
 	"github.com/mjibson/go-dsp/window"
 )
 
-const numSamples = 64
-const maxColumnWidth = 40
-const visualChar = "█"
-const emptyChar = " "
-const peakFalloff = 0.9
+const numSamples = 4608
+const windowWidth = 800
+const windowHeight = 450
+const spectrumSize = 40
+const maxColumnHeight = 450
+const columnWidth = 20
+const peakFalloff = 8.0
 const bitSize = 64
-const spectrumSize = 15
-
-var mutex sync.Mutex
 
 func play(filename string) error {
 
@@ -50,12 +47,12 @@ func play(filename string) error {
 	defer p.Close()
 
 	buf := make([]byte, numSamples)
-
 	freqSpectrum := make([]float64, spectrumSize)
 
-	screen.Clear()
+	rl.InitWindow(windowWidth, windowHeight, "Demo Audio Visualizere")
+	rl.SetTargetFPS(60)
+	for !rl.WindowShouldClose() {
 
-	for {
 		_, err := d.Read(buf)
 		if err != nil {
 			if err == io.EOF {
@@ -64,11 +61,20 @@ func play(filename string) error {
 			return err
 		}
 		updateSpectrumValues(buf, d.SampleRate(), freqSpectrum)
-		go updateScreen(filename, freqSpectrum, &mutex)
 		p.Write(buf) // Playback
+
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+
+		for i, s := range freqSpectrum {
+			rl.DrawRectangleGradientV(int32(i)*columnWidth, windowHeight-int32(s), columnWidth, int32(s), rl.Orange, rl.Green)
+		}
+
+		rl.DrawText("Now Playing: "+filename, 190, 200, 20, rl.LightGray)
+		rl.EndDrawing()
 	}
 
-	screen.Clear()
+	rl.CloseWindow()
 
 	return nil
 }
@@ -91,7 +97,7 @@ func updateSpectrumValues(buffer []byte, sampleRate int, freqSpectrum []float64)
 		fr := real(fftOutput[i])
 		fi := imag(fftOutput[i])
 		magnitude := math.Sqrt(fr*fr + fi*fi)
-		val := math.Min(maxColumnWidth, math.Abs(magnitude))
+		val := math.Min(maxColumnHeight, math.Abs(magnitude))
 		if freqSpectrum[i] > val {
 			freqSpectrum[i] = math.Max(freqSpectrum[i]-peakFalloff, 0.0)
 		} else {
@@ -100,36 +106,14 @@ func updateSpectrumValues(buffer []byte, sampleRate int, freqSpectrum []float64)
 	}
 }
 
-func updateScreen(filename string, spectrum []float64, m *sync.Mutex) {
-
-	m.Lock()
-
-	screen.MoveTopLeft()
-
-	// draw the columns to the console - will replace with a proper GUI library later
-	fmt.Println("> Now playing: ", filename)
-	for i, s := range spectrum {
-		is := int(s)
-		if is == 0 {
-			fmt.Println()
-		} else {
-			fmt.Printf("%02d %02d %s%s\n", i, is,
-				strings.Repeat(visualChar, is),
-				strings.Repeat(emptyChar, maxColumnWidth-is))
-		}
-	}
-
-	m.Unlock()
-}
-
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Printf("Usage: demo_audio_visualizer filename.mp3\n")
 		return
 	}
-
 	filename := os.Args[1]
 	if err := play(filename); err != nil {
 		log.Fatal(err)
 	}
+
 }
